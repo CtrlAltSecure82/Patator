29687775c940f3cda8127163e4d13505f3ac5505
ιтѕ_Ѵιcнч#1337 <66045929+Its-Vichy@users.noreply.github.com>
2021-03-02 21:41:50 +0100

    🍭 Fix error, add features
    Setup / start for Unix and windows

---
+++ Setup/windows/Install.bat
@@ -0,0 +1,8 @@
+@echo off
+title [Vichy] Patator V2 ~ Install...
+cls
+cd ../
+pip install -r requirements.txt
+cls
+echo Install successfully
+pause
---
+++ Setup/windows/Run.bat
@@ -0,0 +1,5 @@
+@echo off
+cls
+cd ../../
+python Patator.py
+pause
